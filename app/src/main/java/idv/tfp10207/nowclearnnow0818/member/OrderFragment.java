4d31bab1f27943aa9212269fa683bd1899dbcfb3
Jacky <a0970421017@gmail.com>
2021-08-20 23:43:43 +0800

    member commit first1

---
+++ app/src/main/java/idv/tfp10207/nowclearnnow0818/member/OrderFragment.java
@@ -0,0 +1,24 @@
+package idv.tfp10207.nowclearnnow0818.member;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import idv.tfp10207.nowclearnnow0818.R;
+
+public class OrderFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_order, container, false);
+    }
+}
