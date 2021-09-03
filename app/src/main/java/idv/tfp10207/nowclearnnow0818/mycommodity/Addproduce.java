fb53c537287e6dbedc08318e71b9767810711277
sofung <sosofung83@gmail.com>
2021-09-03 20:01:01 +0800

    addproduce

---
+++ app/src/main/java/idv/tfp10207/nowclearnnow0818/mycommodity/Addproduce.java
@@ -0,0 +1,9 @@
+package idv.tfp10207.nowclearnnow0818.mycommodity;
+
+public class Addproduce {
+    public static final String PRODUCE = "produce";
+    public static final String PRICE = "price";
+    public static final String DETAIL = "detail";
+    public static final String QUANTITY = "quantity";
+    public static final String PRODUECNAME = "producename";
+}
